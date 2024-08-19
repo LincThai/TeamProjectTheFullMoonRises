3daa5c2386af551bfa4d611c005fafa0f15d56fd
LincThai <78945845+LincThai@users.noreply.github.com>
2024-08-19 17:23:15 +1000

    Bed trigger

---
+++ TheFullMoonRises/Assets/Scripts/BedTrigger.cs
@@ -0,0 +1,27 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEditor.SearchService;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class BedTrigger : MonoBehaviour
+{
+    // variables
+    public int sceneIndex;
+
+    public void Awake()
+    {
+        // get the current scene index amd add 1 for the next scene
+        sceneIndex = SceneManager.GetActiveScene().buildIndex + 1;
+    }
+
+    private void OnTriggerStay(Collider other)
+    {
+        // left mouse click
+        if (Input.GetMouseButtonDown(0))
+        {
+            // move to next scene
+            SceneManager.LoadScene(sceneIndex);
+        }
+    }
+}
