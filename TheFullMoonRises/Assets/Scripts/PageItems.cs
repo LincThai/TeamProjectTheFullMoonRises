757587f742f4c6654588b3ad6bcba61a0bc509d7
LincThai <78945845+LincThai@users.noreply.github.com>
2024-08-20 20:26:50 +1000

    Update PageItems.cs

--- TheFullMoonRises/Assets/Scripts/PageItems.cs
+++ TheFullMoonRises/Assets/Scripts/PageItems.cs
@@ -5,8 +5,10 @@ using UnityEngine.UI;
 
 public class PageItems : MonoBehaviour
 {
+    // set variables
+    // make lists for the checkboxes and their bools
     [SerializeField] public List<Toggle> checkBox;
-    [SerializeField] public List<bool> answers;
+    [SerializeField] public List<bool> playerAnswers;
 
     // Start is called before the first frame update
     void Start()
