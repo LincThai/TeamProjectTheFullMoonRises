62551cbbaa3ac21a03636d6bbf03b3a9a07e9685
LincThai <78945845+LincThai@users.noreply.github.com>
2024-08-22 17:52:24 +1000

    Start New Method of interaction/obj selection

---
+++ TheFullMoonRises/Assets/Scripts/BedInteraction.cs
@@ -0,0 +1,13 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class BedInteraction : MonoBehaviour
+{
+    // set Variables
+    [SerializeField] private string nextLevel;
+    [SerializeField] public bool isInteractable;
+
+
+}
