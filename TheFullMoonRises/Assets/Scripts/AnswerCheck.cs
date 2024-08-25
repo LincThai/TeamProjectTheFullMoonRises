0c24455408f605a3c36f54b5dacfb93158f1d154
LincThai <78945845+LincThai@users.noreply.github.com>
2024-08-25 19:12:08 +1000

    Answer check function & added List functions

--- TheFullMoonRises/Assets/Scripts/AnswerCheck.cs
+++ TheFullMoonRises/Assets/Scripts/AnswerCheck.cs
@@ -8,25 +8,50 @@ public class AnswerCheck : MonoBehaviour
     // lists/arrays for selected objects and answers
     public List<GameObject> selectedObjects;
     public List<string> answers;
-    // int variable counting the number of incorrect
+    // int variable counting the number of correct/incorrect
     public int numOfIncorrect;
+    public int numOfCorrect;
 
     public void CheckAnswers()
     {
+        // variable for the selected object list index
+        int index = 0;
+
+        // check if they don't have same count
         if (answers.Count != selectedObjects.Count)
         {
+            // for every item in answers list
             for (int i = 0; i < answers.Count; i++)
             {
-                if (answers[i] != )
+                // check if the object name in the selectable code is not the same
+                if (answers[i] != selectedObjects[index].GetComponent<Selectable>().objectName)
                 {
-                    
+                    index++;
+                }
+                else 
+                {
+                    // increase the number of correct answers
+                    numOfCorrect++;
                 }
             }
+            // calculate the number of incorrect answers
+            numOfIncorrect = selectedObjects.Count - answers.Count;
         }
     }
 
     public void AddItem(GameObject gameObject)
     {
-        selectedObjects.Add(gameObject);
+        // check if they have the Selectable script
+        if (gameObject.GetComponent<Selectable>() != null)
+        {
+            // add game objects to selected objects list
+            selectedObjects.Add(gameObject);
+        }
+    }
+
+    public void RemoveItem(GameObject gameObject)
+    {
+        // removes from selected objects list
+        selectedObjects.Remove(gameObject);
     }
 }
