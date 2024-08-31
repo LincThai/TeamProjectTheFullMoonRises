52ccfac456512e0594c0bdec4502f534eb3127fa
kuupix <117869224+kuupix@users.noreply.github.com>
2024-08-31 20:03:11 +1000

    Modelled half props in study room. Exported fbx, imported to unity and setup. Added additional code to timer UI script.

--- TheFullMoonRises/Assets/Scripts/Timer.cs
+++ TheFullMoonRises/Assets/Scripts/Timer.cs
@@ -12,7 +12,18 @@ public class Timer : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        remainingTime -= Time.deltaTime;
+        if(remainingTime > 0 )
+        {
+            remainingTime -= Time.deltaTime;
+        }
+        else if (remainingTime <= 0)
+        {
+            remainingTime = 0;
+
+            // call event here after time runs out
+            timerText.color = Color.red;
+        }
+
         int minutes = Mathf.FloorToInt(remainingTime / 60);
         int seconds = Mathf.FloorToInt(remainingTime % 60);
         timerText.text = string.Format("{0:00}:{1:00}", minutes, seconds);
