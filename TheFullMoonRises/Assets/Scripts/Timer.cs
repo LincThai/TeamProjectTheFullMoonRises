3006830f72deb6e4197d1d82b793c9a66a206c9a
LincThai <78945845+LincThai@users.noreply.github.com>
2024-08-31 15:06:42 +1000

    Updating timer to connect with answer check

--- TheFullMoonRises/Assets/Scripts/Timer.cs
+++ TheFullMoonRises/Assets/Scripts/Timer.cs
@@ -5,16 +5,48 @@ using TMPro;
 
 public class Timer : MonoBehaviour
 {
+    // set variables
+    // timer variables
+    [Header("Timer")]
     [SerializeField] TextMeshProUGUI timerText;
     [SerializeField] float remainingTime;
+    [SerializeField] float maxTime = 3;
 
+    // punishment variables
+    [Header("Time Punishment")]
+    [SerializeField] float timeReduction = 1;
+    [SerializeField] bool isPunished = false;
+    [SerializeField] int numOfTtimes = 0;
+
+
+
+    private void Awake()
+    {
+        // set the remaining time to the max time limit
+        remainingTime = maxTime;
+
+        // call function to check for punishment time reduction
+    }
 
     // Update is called once per frame
     void Update()
     {
+        // reduce time
         remainingTime -= Time.deltaTime;
+        // set variables for minutes and seconds
         int minutes = Mathf.FloorToInt(remainingTime / 60);
         int seconds = Mathf.FloorToInt(remainingTime % 60);
+        // display as text
         timerText.text = string.Format("{0:00}:{1:00}", minutes, seconds);
+    }
+
+    public void PTimeReduction()
+    {
+        // checks if there is a punishment
+        if (isPunished)
+        {
+            // reduce remaining time by time reduction multiplied by the number of times
+            remainingTime -= timeReduction * numOfTtimes;
+        }
     }
 }
