673ee9a94665a4e5baae92a4d91b7cb15ca942d0
LF-MasterSpark <3083811590@qq.com>
2024-09-07 22:32:32 +1000

    Ui text Backup copy

--- TheFullMoonRises/Assets/Scripts/MainMenu.cs
+++ TheFullMoonRises/Assets/Scripts/MainMenu.cs
@@ -4,12 +4,14 @@ using UnityEngine;
 using UnityEngine.SceneManagement;
 public class MainMenu : MonoBehaviour
 {
-    
+    //// Define a public method called PlayGame
     public void PlayGame()
     {
+        // Get the index of the current active scene and load the next scene
         SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
     }
 
+    //Quit the game
     public void QuitGame ()
     {
         Debug.Log("QUIT!");
