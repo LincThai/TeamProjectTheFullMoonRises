a9cc451a5891cbc5318d8aac3e3d02ea3bab2241
LF-MasterSpark <3083811590@qq.com>
2024-09-08 15:48:00 +1000

    UI Test (complete)
    Start menu,
    Settings menu,
    Pause menu    and
    Billing Menu.
    How to use:
    It's a good idea to check the UI Programming in the notion Project Wiki before copying, debugging, and merging canvas with all built-in menu functions.
    If you do not understand or need to add, please ask me directly.

--- TheFullMoonRises/Assets/Scripts/PauseMenu.cs
+++ TheFullMoonRises/Assets/Scripts/PauseMenu.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class PauseMenu : MonoBehaviour
 {
@@ -8,9 +9,23 @@ public class PauseMenu : MonoBehaviour
     public static bool GameIsPaused = false;
 
     public GameObject pauseMenuUI;
+    public GameObject billingMenuUI;
+
+    private float gameTime = 0f;
+    public float targetTime = 10f;
 
     void Update()
     {
+        if (!GameIsPaused)
+        {
+            gameTime += Time.deltaTime;
+            // 检查是否达到目标时间
+            if (gameTime >= targetTime)
+            {
+                ShowBillingMenu();
+            }
+        }
+
         if (Input.GetKeyDown(KeyCode.Escape))
         {
             if (GameIsPaused) 
@@ -23,6 +38,7 @@ public class PauseMenu : MonoBehaviour
         }
     }
 
+    // Resume pausing and hiding UI
     void Resume()
     {
         pauseMenuUI.SetActive(false);
@@ -30,10 +46,33 @@ public class PauseMenu : MonoBehaviour
         GameIsPaused = false;
     }
 
+    // Method of pausing the game
     void Pause()
     {
         pauseMenuUI.SetActive(true);
+        //Stop time
         Time.timeScale = 0f;
+        GameIsPaused = true;
+    }
+
+    public void LoadMenu()
+    {
+        SceneManager.LoadScene("UI test_level2");
+    }
+
+    public void QuitGame()
+    {
+        Debug.Log("quitting game..");
+        Application.Quit();
+    }
+   
+    void ShowBillingMenu()
+    {
+        // 显示结算菜单UI
+        billingMenuUI.SetActive(true);
+        // 停止游戏时间
+        Time.timeScale = 0f;
+        // 设置游戏状态为暂停
         GameIsPaused = true;
     }
 }
