154ac27b5fc23aa1794c2917d639498c12cc8efd
LincThai <78945845+LincThai@users.noreply.github.com>
2024-09-26 12:24:45 +1000

    Completed the rework of AnswerCheck for Bug Fix

--- TheFullMoonRises/Assets/Scripts/AnswerCheck.cs
+++ TheFullMoonRises/Assets/Scripts/AnswerCheck.cs
@@ -17,69 +17,37 @@ public class AnswerCheck : MonoBehaviour
     public int numOfCorrect;
 
     public void CheckAnswers()
-    {          
+    {   
+        // if selected objects list has objects in it
         if (selectedObjects.Count > 0)
         {
+            // set values to zero
             numOfCorrect = 0;
             numOfIncorrect = 0;
 
             for (int i = 0; i < answers.Count; i++)
             {
+                // using the count function in system.linq compare the items in the lists and count when they are the same or different
+                // and add them to the variables
                 numOfCorrect += selectedObjects.Count(x => x.GetComponent<Selectable>().objectName == answers[i]);
                 numOfIncorrect += selectedObjects.Count(x => x.GetComponent<Selectable>().objectName != answers[i]);
-
-                //// check if the object name in the selectable code is not the same
-                //if (answers[i] != selectedObjects[index].GetComponent<Selectable>().objectName)
-                //{
-                //    // increase index
-                //    index++;
-                //    //Debug.Log(index);
-                //    // check if index is higher than the count of selectedObjects list 
-                //    if (index >= selectedObjects.Count)
-                //    {
-                //        // set index equal to the selectedObjects count - 1 as lists start from 0
-                //        // meaning the size of a list = n-1
-                //        index = selectedObjects.Count - 1;
-                //        //Debug.Log(index);
-                //    }
-                //}
-                //else
-                //{
-                //    // increase the number of correct answers
-                //    numOfCorrect++;
-                //    // check if the number
-                //    if (numOfCorrect >= answers.Count)
-                //    {
-                //        // set value to max
-                //        numOfCorrect = answers.Count;
-                //    }
-                //    Debug.Log("Correct " + numOfCorrect);
-                //    // send value to game manager
-                //    GameManager.Instance.numOfCorrect = numOfCorrect;
-                //}
             }
 
+            // Send values to game Manager
             GameManager.Instance.numOfCorrect = numOfCorrect;
             GameManager.Instance.numOfIncorrect = numOfIncorrect;
-        }
 
-        if (answers.Count <= selectedObjects.Count)
-        {
-            // calculate the number of incorrect answers
-            numOfIncorrect = selectedObjects.Count - answers.Count;
-            Debug.Log( "Incorrect " + numOfIncorrect);
+            if (numOfIncorrect > 0)
+            {
+                // set bool to true
+                GameManager.Instance.willPunish = true;
+            }
+            else
+            {
+                // set bool to true
+                GameManager.Instance.willPunish = false;
+            }
         }
-        else
-        { 
-            numOfIncorrect = 0; 
-            Debug.Log("Incorrect " + numOfIncorrect); 
-        }
-
-        // send value to game manager
-        GameManager.Instance.numOfIncorrect = numOfIncorrect;
-
-        // set bool to true
-        GameManager.Instance.willPunish = true;
     }
 
     public void AddItem(GameObject gameObject)
